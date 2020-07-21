4236dac51014abcdcac5cf05559335439703e5f1
chaima-slama <68553936+chaima-slama@users.noreply.github.com>
2020-07-21 16:36:05 +0200

    Update tile.js

--- tile.js
+++ tile.js
@@ -1,55 +1,240 @@
-(function () {
-	let tmpl = document.createElement("template");
-tmpl.innerHTML = `
-        <h1>Hello World</h1>
+(function() {
+    let _shadowRoot;
+    let _id;
+    let _password;
+
+    let tmpl = document.createElement("template");
+    tmpl.innerHTML = `
+        <style>
+        </style>
+        <div id="ui5_content" name="ui5_content">
+         <slot name="content"></slot>
+        </div>
+        <script id="oView" name="oView" type="sapui5/xmlview">
+            <mvc:View
+			    controllerName="myView.Template"
+				xmlns:l="sap.ui.layout"
+				xmlns:mvc="sap.ui.core.mvc"
+				xmlns="sap.m">
+				<l:VerticalLayout
+					class="sapUiContentPadding"
+					width="100%">
+					<l:content>
+						<Input
+							id="passwordInput"
+							type="Password"
+							placeholder="Enter password ..." liveChange="onButtonPress"/>
+					</l:content>
+				</l:VerticalLayout>
+			</mvc:View>
+        </script>        
     `;
 
-customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    class InputPassword extends HTMLElement {
 
-constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
-		}
+        constructor() {
+            super();
 
+            _shadowRoot = this.attachShadow({
+                mode: "open"
+            });
+            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
+            _id = createGuid();
+
+            _shadowRoot.querySelector("#oView").id = _id + "_oView";
+
+            this._export_settings = {};
+            this._export_settings.password = "";
+
+            this.addEventListener("click", event => {
+                console.log('click');
+            });
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
+        connectedCallback() {
+            try {
+                if (window.commonApp) {
+                    let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
+
+                    if (outlineContainer && outlineContainer.getReactProps) {
+                        let parseReactState = state => {
+                            let components = {};
+
+                            let globalState = state.globalState;
+                            let instances = globalState.instances;
+                            let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
+                            let names = app.names;
+
+                            for (let key in names) {
+                                let name = names[key];
+
+                                let obj = JSON.parse(key).pop();
+                                let type = Object.keys(obj)[0];
+                                let id = obj[type];
+
+                                components[id] = {
+                                    type: type,
+                                    name: name
+                                };
+                            }
+
+                            for (let componentId in components) {
+                                let component = components[componentId];
+                            }
+
+                            let metadata = JSON.stringify({
+                                components: components,
+                                vars: app.globalVars
+                            });
+
+                            if (metadata != this.metadata) {
+                                this.metadata = metadata;
+
+                                this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                                    detail: {
+                                        properties: {
+                                            metadata: metadata
+                                        }
+                                    }
+                                }));
+                            }
+                        };
+
+                        let subscribeReactStore = store => {
+                            this._subscription = store.subscribe({
+                                effect: state => {
+                                    parseReactState(state);
+                                    return {
+                                        result: 1
+                                    };
+                                }
+                            });
+                        };
+
+                        let props = outlineContainer.getReactProps();
+                        if (props) {
+                            subscribeReactStore(props.store);
+                        } else {
+                            let oldRenderReactComponent = outlineContainer.renderReactComponent;
+                            outlineContainer.renderReactComponent = e => {
+                                let props = outlineContainer.getReactProps();
+                                subscribeReactStore(props.store);
+
+                                oldRenderReactComponent.call(outlineContainer, e);
+                            }
+                        }
+                    }
+                }
+            } catch (e) {}
         }
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
-
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.redraw();
+        disconnectedCallback() {
+            if (this._subscription) { 
+                this._subscription();
+                this._subscription = null;
             }
         }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
+
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            if ("designMode" in changedProperties) {
+                this._designMode = changedProperties["designMode"];
+            }
         }
 
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-        
+        onCustomWidgetAfterUpdate(changedProperties) {
+            loadthis(this);
         }
-        */
 
-        redraw(){
+        _firePropertiesChanged() {
+            this.password = "";
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        password: this.password
+                    }
+                }
+            }));
         }
-    });
+
+        // SETTINGS
+        get password() {
+            return this._export_settings.password;
+        }
+        set password(value) {
+            value = _password;
+            this._export_settings.password = value;
+        }
+
+        static get observedAttributes() {
+            return [
+                "password"
+            ];
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (oldValue != newValue) {
+                this[name] = newValue;
+            }
+        }
+
+    }
+    customElements.define("com-fd-djaja-sap-sac-inputpassword", InputPassword);
+
+    // UTILS
+    function loadthis(that) {
+        var that_ = that;
+      
+        let content = document.createElement('div');
+        content.slot = "content";
+        that_.appendChild(content);
+
+        sap.ui.getCore().attachInit(function() {
+            "use strict";
+
+            //### Controller ###
+            sap.ui.define([
+                "jquery.sap.global",
+                "sap/ui/core/mvc/Controller"
+            ], function(jQuery, Controller) {
+                "use strict";
+
+                return Controller.extend("myView.Template", {
+                    onButtonPress: function(oEvent) {
+                        _password = oView.byId("passwordInput").getValue();
+                        that._firePropertiesChanged();
+                        console.log(_password);
+
+                        this.settings = {};
+                        this.settings.password = "";
+
+                        that.dispatchEvent(new CustomEvent("onStart", {
+                            detail: {
+                                settings: this.settings
+                            }
+                        }));
+                    } 
+                });
+            });
+
+            //### THE APP: place the XMLView somewhere into DOM ###
+            var oView  = sap.ui.xmlview({
+                viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
+            });
+            oView.placeAt(content);
+
+
+            if (that_._designMode) {
+                oView.byId("passwordInput").setEnabled(false);
+            }
+        });
+    }
+
+    function createGuid() {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+            let r = Math.random() * 16 | 0,
+                v = c === "x" ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }  
 })();
